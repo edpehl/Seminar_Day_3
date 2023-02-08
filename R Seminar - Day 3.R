82c05a331d809b16d6e6cb8836d625ae9f7e78ea
edpehl <edina.pehlic@uzh.ch>
2023-02-08 10:17:45 +0100

    first commit

---
+++ R Seminar - Day 3.R
@@ -0,0 +1,46 @@
+##  R Seminar Day 3
+#Exercises Day 3: GitHub
+
+### clear working space
+rm(list=ls())
+
+### remove an object
+rm(x)
+
+### set working directory
+# what I usually do: session--set working directory--choose directory
+setwd("C:/Users/edina/Desktop/Assessment year/Master/FS23/R - a non-technical introduction to big data techniques, team work and interactive visualization/Day 3")
+getwd()
+
+### 0 Operating packages
+install.packages("data.table")
+library("data.table")
+install.packages("tidyverse")
+library("tidyverse")
+install.packages("lubridate")
+library("lubridate")
+
+#### Exercises Day 3
+
+### 17) Why version control?
+## 1. Install Git on your computer: https://git-scm.com
+## 2. Create a Github account: https://github.com
+
+### 18) Let’s get started with Git
+## 1. Create an RSA key in RStudio and add it to your GitHub account
+#Initialize a local Git repository within your created project
+  #run git init in terminal; only enter; need the right folder
+
+
+
+## 2. Create a new public repository on GitHub.
+
+## 3. Create a RStudio project with version control, that is connected to the new
+##  repository on GitHub
+
+## 4. Create a test file, add some changes, and push it to your GitHub repository
+
+## 5. Clone the following repository to your computer: https://github.com/octocat/Spoon-Knife
+
+
+
