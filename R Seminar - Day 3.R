b321e0594b68271934cb202310ec461515488ce6
edpehl <edina.pehlic@uzh.ch>
2023-02-08 10:45:27 +0100

    second commit

--- R Seminar - Day 3.R
+++ R Seminar - Day 3.R
@@ -31,14 +31,13 @@ library("lubridate")
 #Initialize a local Git repository within your created project
   #run git init in terminal; only enter; need the right folder
 
-
-
 ## 2. Create a new public repository on GitHub.
 
 ## 3. Create a RStudio project with version control, that is connected to the new
 ##  repository on GitHub
 
 ## 4. Create a test file, add some changes, and push it to your GitHub repository
+#change 
 
 ## 5. Clone the following repository to your computer: https://github.com/octocat/Spoon-Knife
 
